c3723a0f9db1068e7a7cfdc9da3f8754f473c7f4
TistaMilani <francesco.rocchiccioli.02@gmail.com>
2025-07-08 20:55:35 +0200

    Added token duplication and unpaid fee PoC

--- contracts/simplified_deflationary_token/hardhat/test.js
+++ contracts/simplified_deflationary_token/hardhat/test.js
@@ -2,28 +2,44 @@ const {
     loadFixture,
   } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
   const { expect } = require("chai");
-  
-  describe("DepositERC20_v1", function() {
+
+  describe("SimplifiedDeflationaryToken", function() {
     async function deployContractv1() {
-      const [receiver] = await ethers.getSigners();
-  
-      const ERC20Factory = await ethers.getContractFactory("ERC20");
-      const ERC20 = await ERC20Factory.deploy(100);
-      const DepositERC20Factory = await ethers.getContractFactory("TokenTransfer");
-      const DepositERC20 = await DepositERC20Factory.deploy(ERC20.getAddress());
-  
-      return { DepositERC20, ERC20, receiver };
+      const [owner, sender, receiver] = await ethers.getSigners();
+
+      const sdtv1 = await ethers.getContractFactory("SimplifiedDeflationaryTokenV1");
+      const SDTv1 = await sdtv1.connect(owner).deploy();
+
+      const sdtv2 = await ethers.getContractFactory("SimplifiedDeflationaryTokenV2");
+      const SDTv2 = await sdtv2.connect(owner).deploy();
+
+      return { SDTv1, SDTv2, owner, sender, receiver };
     }
-      it("wd-leq-init-bal: the overall withdrawn amount exceed the initial deposit", async function(){
-        const { DepositERC20, ERC20, receiver } = await loadFixture(deployContractv1);
-        await DepositERC20.deposit();
+      it("Token duplication", async function(){
+        const { SDTv1, owner, sender, receiver } = await loadFixture(deployContractv1);
+        await SDTv1.connect(owner).transfer(sender.address, 100);
+        
+        await SDTv1.connect(sender).transfer(receiver.address, 100);
+        
+        let balanceOwner = await SDTv1.balanceOf(owner.address);
+        let balanceSender = await SDTv1.balanceOf(sender.address);
+        let balanceReceiver = await SDTv1.balanceOf(receiver.address);
+        
+        expect(
+          balanceReceiver+balanceOwner+balanceSender
+        ).to.be.gt(await SDTv1.totalSupply());
+      })
+      
+      it("Unpaid Fee", async function(){
+        const { SDTv2, owner, sender, receiver } = await loadFixture(deployContractv1);
+        await SDTv2.connect(owner).transfer(sender.address, 100);
+        
+        let balanceBeforeOwner = await SDTv2.balanceOf(owner.address);
 
-        await DepositERC20.connect(receiver).withdraw(100);
-        await ERC20.connect(receiver).transfer(DepositERC20, 100);
-        await DepositERC20.connect(receiver).withdraw(100);
-
-        expect(await DepositERC20.getSent()).to.be.equal(200);  
-
-
+        await SDTv2.connect(sender).transfer(receiver.address, 9);
+      
+        expect(
+          await SDTv2.balanceOf(owner.address)
+        ).to.be.equal(balanceBeforeOwner);
       })
   })
